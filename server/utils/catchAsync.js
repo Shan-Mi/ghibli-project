6c3c5c7e152f03981caedd5254e2403fea089f59
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-01-28 23:33:24 +0100

    Make catchAsync even cleaner

--- server/utils/catchAsync.js
+++ server/utils/catchAsync.js
@@ -1,5 +1,3 @@
-const catchAsync = (fn) => {
-  return (req, res, next) => fn(req, res, next).catch(next);
-};
+const catchAsync = (fn) => (req, res, next) => fn(req, res, next).catch(next);
 
 export default catchAsync;
