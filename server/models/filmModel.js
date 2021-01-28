81a2733a141aa70ef82481056f64fbb0c20df56e
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-01-28 15:00:06 +0100

    Edit films model and json file for import db

--- server/models/filmModel.js
+++ server/models/filmModel.js
@@ -3,13 +3,14 @@ import mongoose from "mongoose";
 const filmSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    name: { type: String, required: true },
     trailer: { type: String, required: true },
     rating: { type: Number, required: true },
-    director: { type: String, required: true },
+    directors: { type: String, required: true },
     description: { type: String, required: true },
-    yearofproduction: { type: String, required: true },
-    runtime: { type: String, required: true },
+    releasedDate: { type: String, required: true },
+    runtime: { type: Date, required: true },
+    imageCover: { type: String, required: true },
+    images: [{ type: String, required: true }],
     review: [{ type: mongoose.Schema.Types.ObjectId, ref: "review" }],
   },
   { timestamps: true }
