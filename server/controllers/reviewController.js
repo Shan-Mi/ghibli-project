e5da5c6c7479ad3f3dac0f3c01d957e5cfdf21b4
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-01-28 23:36:05 +0100

    Add likeReview func, need refactor later

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -7,7 +7,7 @@ import * as factory from "./handlerFactory.js";
 //   // console.log(reviews);
 //   res.status(200).json(reviews);
 // });
-export const getReviews = factory.getAll(Review, 'reviews');
+export const getReviews = factory.getAll(Review, "reviews");
 
 export const createReview = catchAsync(async (req, res) => {
   // res.send("Created review");
@@ -16,5 +16,23 @@ export const createReview = catchAsync(async (req, res) => {
   await newReview.save();
   res.status(200).json(newReview);
 });
+
+export const likeReview = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No review with id: ${id}`);
+
+  const review = await Review.findById(id);
+
+  // TODO: make sure each user can only click once, likeCount can only increase by 1 for each user.
+  const updatedReview = await Review.findByIdAndUpdate(
+    id,
+    { likeCount: review.likeCount + 1 },
+    { new: true }
+  );
+
+  res.json(updatedReview);
+};
 
 // factory.createOne(Review);
