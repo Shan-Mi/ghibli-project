ceacd850dc4ab4af69de340fc1784a35811dd340
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-01-31 22:31:05 +0100

    Preparation for auth route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,23 @@
 import express from "express";
+import {
+  signup,
+  login,
+  logout,
+  forgotPassword,
+  resetPassword,
+} from "../controllers/authController.js";
 import { getAllUsers, getOneUser } from "../controllers/userController.js";
 
 const router = express.Router();
 
 router.get("/", getAllUsers);
 router.get("/:id", getOneUser);
+
+// new added
+router.post("/signup", signup);
+router.post("/login", login);
+router.get("/logout", logout);
+router.post("/forgotPassword", forgotPassword);
+router.patch("/resetPassword/:token", resetPassword);
 
 export default router;
