224e71fcc4229455dee7f847831a73753b4ca58d
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-01 11:38:14 +0100

    Fix get one review

--- server/controllers/reviewController.js
+++ server/controllers/reviewController.js
@@ -26,13 +26,46 @@ export const likeReview = async (req, res) => {
   const review = await Review.findById(id);
 
   // TODO: make sure each user can only click once, likeCount can only increase by 1 for each user.
+  // we push this user's id to likedBy array on review model, only if that id doesn't already exist there.
+  // then we calc length of that array to get back likedCount
+  // also populate that.
   const updatedReview = await Review.findByIdAndUpdate(
     id,
     { likeCount: review.likeCount + 1 },
     { new: true }
   );
 
+  // aggregate new piece of data.
+  const newReview = await Review.aggregate([
+    {
+      $project: {
+        title: 1,
+        content: 1,
+        createdAt: 1,
+        likedCount: { $sum: { $size: "$likedBy" } },
+      },
+    },
+  ]);
+
+  const query = { id: req.params.id };
+  const update = [
+    {
+      $set: {
+        likedBy: {
+          $setUnion: [{ $ifNull: ["$likedBy", []] }, [req.params.id]],
+        },
+      },
+    },
+    { $set: { likedCount: { $size: "$likedBy" } } },
+  ];
+  const options = { upsert: true, new: true, setDefaultsOnInsert: true };
+  await skillSchema.findOneAndUpdate(query, update, options);
+
   res.json(updatedReview);
 };
 
 // factory.createOne(Review);
+export const getReview = factory.getOne(Review, "review");
+
+export const updateReview = () => {};
+export const deleteReview = () => {};
