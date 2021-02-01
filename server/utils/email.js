3b039c9f46ab2ba82e057edb6ffcef2ea017dcac
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-01 14:46:24 +0100

    Sending mail template

--- server/utils/email.js
+++ server/utils/email.js
@@ -1,9 +1,9 @@
-import nodemailer from 'nodemailer';
-// const nodemailer = require("nodemailer");
-// import pug from 'pug';
-// const pug = require("pug");
-import {htmlToText} from 'html-to-text';
-// const { htmlToText } = require("html-to-text");
+import nodemailer from "nodemailer";
+import pug from "pug";
+import { htmlToText } from "html-to-text";
+import path from "path";
+
+const __dirname = path.resolve();
 
 // new Email(user, url).sendWelcome() .sendReset
 export default class Email {
@@ -41,39 +41,39 @@ export default class Email {
 
   // Send the actual email
 
-//   async send(template, subject) {
-//     // 1) render html based on a pug template
-//     const html = pug.renderFile(
-//       `${__dirname}/../views/emails/${template}.pug`,
-//       {
-//         firstName: this.firstName,
-//         url: this.url,
-//         subject,
-//       }
-//     );
-//     // 2) define email options
-//     const mailOptions = {
-//       from: this.from,
-//       to: this.to,
-//       subject,
-//       html,
-//       text: htmlToText(html),
-//       // html: we can transfer text to html here
-//     };
+  async send(template, subject) {
+    // 1) render html based on a pug template
+    const html = pug.renderFile(
+      `${__dirname}/server/views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      }
+    );
+    // 2) define email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText(html),
+      // html: we can transfer text to html here
+    };
 
-//     // 3) create a transport and send email
-//     await this.newTransport().sendMail(mailOptions);
-//     // await transport.sendMail(mailOptions);
-//   }
+    // 3) create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+    // await transport.sendMail(mailOptions);
+  }
 
-//   async sendWelcome() {
-//     await this.send("welcome", "Welcome to the Natours Family!");
-//   }
+  async sendWelcome() {
+    await this.send("welcome", "Welcome to the Ghibli Fan's Family!");
+  }
 
-//   async sendPasswordReset() {
-//     await this.send(
-//       "passwordReset",
-//       "Your password reset token (valid for only 10 minutes)"
-//     );
-//   }
-};
+  async sendPasswordReset() {
+    await this.send(
+      "passwordReset",
+      "Your password reset token (valid for only 10 minutes)"
+    );
+  }
+}
