f609b5dcda50f82c80ee80a024415a93c4b735f9
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-01 11:38:58 +0100

    Fix nested route for review for film

--- server/routes/filmRoutes.js
+++ server/routes/filmRoutes.js
@@ -6,7 +6,9 @@ const router = express.Router();
 
 router.get("/", getFilms);
 router.get("/:id", getOneFilm);
+// router.get("?title=[]")  -> use this one to search for one film. but maybe we can do that from frontend
 
-router.use("/:id/reviews", reviewRouter);
+// nested route for each review for each film
+router.use("/:filmId/reviews", reviewRouter);
 
 export default router;
