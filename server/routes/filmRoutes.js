fa2d0f6b3df94d33032d12ee6dca3d476a30fe4d
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-01 22:45:46 +0100

    Clean review controller

--- server/routes/filmRoutes.js
+++ server/routes/filmRoutes.js
@@ -6,7 +6,6 @@ const router = express.Router();
 
 router.get("/", getFilms);
 router.get("/:id", getOneFilm);
-// router.get("?title=[]")  -> use this one to search for one film. but maybe we can do that from frontend
 
 // nested route for each review for each film
 router.use("/:filmId/reviews", reviewRouter);
