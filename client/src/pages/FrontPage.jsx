757548102150d3a7797ff067bc55c51840b38fc6
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-04 10:58:14 +0100

    Fix className issue

--- client/src/pages/FrontPage.jsx
+++ client/src/pages/FrontPage.jsx
@@ -19,9 +19,9 @@ const FrontPage = () => {
   }, [setFilms]);
 
   return (
-    <main class="flex-grow flex justify-center items-center">
-      <div class="mx-auto px-4 sm:px-8 py-2 text-center">
-        <div class="grid grid-cols-6 gap-4 items-start mt-8 mx-auto px-8 max-w-7xl">
+    <main className="flex-grow flex justify-center items-center">
+      <div className="mx-auto px-4 sm:px-8 py-2 text-center">
+        <div className="grid grid-cols-6 gap-4 items-start mt-8 mx-auto px-8 max-w-7xl">
           {films.map((film, index) => (
             <FilmReview film={film} key={index} />
           ))}
