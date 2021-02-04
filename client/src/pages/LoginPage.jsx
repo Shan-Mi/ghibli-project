abb10cba999bb9fd7aa1fd948dabfe5653506190
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-04 10:58:41 +0100

    Import and render out loginForm

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import LoginForm from "../components/LoginForm";
 
 const LoginPage = () => {
   return (
     <div>
-      This is login page.
+      <LoginForm />
     </div>
-  )
-}
+  );
+};
 
-export default LoginPage
+export default LoginPage;
