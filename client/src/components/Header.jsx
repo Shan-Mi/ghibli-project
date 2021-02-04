f12adac9e3eb029f0c72631e59782d7b05b0e29e
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-04 10:56:55 +0100

    Check if user login to render out diff btn

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { FiMenu } from "react-icons/fi";
 import GhibliLogo from "../assets/GhibliLogo.svg";
+import { GhibliContext } from "../context/GlobalContext";
+import { logout } from "../api";
 
 const Header = () => {
+  const { user } = useContext(GhibliContext);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const res = Object.keys(user).length ? true : false;
+    setIsLoggedIn(res);
+  }, [user]);
+
+  const handleLogout = () => {
+    logout();
+    setIsLoggedIn(false);
+  };
+
   return (
     <nav className="flex items-center bg-gray-800 p-3 flex-wrap">
       <Link to="/" className="p-2 mr-4 inline-flex items-center">
-        <img src={GhibliLogo} fill alt="Ghibli Studio Logo" />
+        <img src={GhibliLogo} alt="Ghibli Studio Logo" />
         <span className="text-xl text-white font-bold uppercase tracking-wide self-end">
           Ghibli Project
         </span>
@@ -58,12 +73,21 @@ const Header = () => {
             <span>Register</span>
           </Link>
 
-          <Link
-            to="/login"
-            className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white"
-          >
-            <span>Login</span>
-          </Link>
+          {!isLoggedIn ? (
+            <Link
+              to="/login"
+              className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white"
+            >
+              <span>Login</span>
+            </Link>
+          ) : (
+            <button
+              onClick={handleLogout}
+              className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-gray-400 items-center justify-center hover:bg-gray-900 hover:text-white"
+            >
+              <span>Logout</span>
+            </button>
+          )}
         </div>
       </div>
     </nav>
