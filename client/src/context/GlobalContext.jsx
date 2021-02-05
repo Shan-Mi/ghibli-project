f5eb8f65a21eec78074f99879670c98076699a7f
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-05 11:03:07 +0100

    Store user/ token in localstorage in globalcontext

--- client/src/context/GlobalContext.jsx
+++ client/src/context/GlobalContext.jsx
@@ -1,13 +1,25 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 
 export const GhibliContext = createContext({});
 
 const GlobalContext = ({ children }) => {
   const [user, setUser] = useState({});
   const [films, setFilms] = useState([]);
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    if (localStorage.getItem("user")) {
+      setUser(JSON.parse(localStorage.getItem("user")));
+    }
+    if (localStorage.getItem("token")) {
+      setToken(JSON.parse(localStorage.getItem("token")));
+    }
+  }, []);
 
   return (
-    <GhibliContext.Provider value={{ user, setUser, films, setFilms }}>
+    <GhibliContext.Provider
+      value={{ user, setUser, films, setFilms, token, setToken }}
+    >
       {children}
     </GhibliContext.Provider>
   );
