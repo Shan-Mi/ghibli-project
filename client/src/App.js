74550f2938d2a968fa484fe66feb54dce658a743
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-05 11:00:36 +0100

    Add redirect for login

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Header from "./components/Header";
 import FrontPage from "./pages/FrontPage";
 import FilmPage from "./pages/FilmPage";
@@ -7,8 +7,27 @@ import RegisterPage from "./pages/RegisterPage";
 import ProfilePage from "./pages/ProfilePage";
 import Page404 from "./pages/Page404";
 import About from "./pages/About";
+import { useContext } from "react";
+import { GhibliContext } from "./context/GlobalContext";
 
+// import axios from 'axios'
+// import { URL } from "./constants";
+// axios.defaults.withCredentials = true
+
+// URL
+
+// const createCookie = () => {
+//   axios.get('http://localhost:4000',{ withCredentials: true }).then((res) =>{
+//     console.log(res.data)
+//   })
+// }
+// const deleteCookie = () => {
+//   axios.get('http://localhost:4000/deleteCookie',{ withCredentials: true }).then((res) =>{
+//     console.log(res.data)
+//   })
+// }
 function App() {
+  const { user } = useContext(GhibliContext);
   return (
     <>
       <Header />
@@ -16,7 +35,11 @@ function App() {
         <Route path="/" exact component={FrontPage} />
         <Route path="/about" component={About} />
         <Route path="/films/:id" component={FilmPage} />
-        <Route path="/login" component={LoginPage} />
+
+        <Route exact path="/login">
+          {Object.keys(user).length ? <Redirect to="/" /> : <LoginPage />}
+        </Route>
+
         <Route path="/register" component={RegisterPage} />
         <Route path="/profile" component={ProfilePage} />
         <Route path="*" component={Page404} />
