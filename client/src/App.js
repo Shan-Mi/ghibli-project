257d6c0bcf444e4e20012fb9e64341c4b41fe30c
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-05 20:46:45 +0100

    Add resetpassword related pages and routes

--- client/src/App.js
+++ client/src/App.js
@@ -7,8 +7,10 @@ import RegisterPage from "./pages/RegisterPage";
 import ProfilePage from "./pages/ProfilePage";
 import Page404 from "./pages/Page404";
 import About from "./pages/About";
-import { useContext } from "react";
+import ResetPage from "./pages/ResetPage";
 import { GhibliContext } from "./context/GlobalContext";
+import { useContext } from "react";
+import ResetPasswordPage from "./pages/ResetPasswordPage";
 
 // import axios from 'axios'
 // import { URL } from "./constants";
@@ -41,6 +43,8 @@ function App() {
         </Route>
 
         <Route path="/register" component={RegisterPage} />
+        <Route path="/resetPassword/:token" component={ResetPasswordPage} />
+        <Route path="/sendResetPassword" component={ResetPage} />
         <Route path="/profile" component={ProfilePage} />
         <Route path="*" component={Page404} />
       </Switch>
