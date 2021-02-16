93df634a18d176e20efec519c261a3768804793f
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-16 10:30:16 +0100

    Run getFilms conditionally

--- client/src/components/FilmDetail.jsx
+++ client/src/components/FilmDetail.jsx
@@ -21,7 +21,7 @@ const FilmDetail = () => {
       setFilm(currentFilm);
     };
     // TODO: NEED TO avoid multiple fetching data, if data is in context, we don't need to fetch from api.
-    getFilms();
+    !films && getFilms();
     // console.log("you should run");
   }, []);
 
