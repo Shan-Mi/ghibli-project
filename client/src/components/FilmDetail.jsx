495b6f9b1302fb6bd72c035b61a88bfe704a0191
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-16 10:45:06 +0100

    Revert mistake

--- client/src/components/FilmDetail.jsx
+++ client/src/components/FilmDetail.jsx
@@ -21,7 +21,7 @@ const FilmDetail = () => {
       setFilm(currentFilm);
     };
     // TODO: NEED TO avoid multiple fetching data, if data is in context, we don't need to fetch from api.
-    !films && getFilms();
+    getFilms();
     // console.log("you should run");
   }, [setFilms, slug]);
 
