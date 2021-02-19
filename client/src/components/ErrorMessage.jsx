997c53327362d77b3f10083de52092d2aff32247
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-19 12:46:57 +0100

    Settimeout cause rerender, comment out for now

--- client/src/components/ErrorMessage.jsx
+++ client/src/components/ErrorMessage.jsx
@@ -10,13 +10,13 @@ const ErrorMessage = () => {
     setError({ message: "", hidden: true });
   };
 
-  useEffect(() => {
-    // hide error message after 4500 ms if the user is not closing this component by themselves.
-    const timer = setTimeout(() => {
-      setError({ ...error, hidden: true });
-    }, 4500);
-    return () => clearTimeout(timer);
-  }, [error, setError]);
+  // useEffect(() => {
+  //   // hide error message after 4500 ms if the user is not closing this component by themselves.
+  //   const timer = setTimeout(() => {
+  //     setError({ ...error, hidden: true });
+  //   }, 4500);
+  //   return () => clearTimeout(timer);
+  // }, [error, setError]);
 
   return (
     <div
