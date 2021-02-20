e2ee882cb626c3caea047cd239e62f39041dd08b
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-20 22:54:31 +0100

    Add restrictTo for creae/update film

--- server/routes/filmRoutes.js
+++ server/routes/filmRoutes.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { restrictTo } from "../controllers/authController.js";
 import {
   getFilms,
   getOneFilm,
@@ -9,9 +10,9 @@ import reviewRouter from "../routes/reviewRoutes.js";
 
 const router = express.Router();
 
-router.route("/").get(getFilms).post(createOneFilm);
+router.route("/").get(getFilms).post(restrictTo("admin"), createOneFilm);
 
-router.route("/:id").get(getOneFilm).patch(updateOneFilm);
+router.route("/:id").get(getOneFilm).patch(restrictTo("admin"), updateOneFilm);
 
 // nested route for each review for each film
 router.use("/:filmId/reviews", reviewRouter);
