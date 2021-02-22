73f5259669f79b43462a2aac8228479ca7b59d46
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-23 00:05:40 +0100

    Add change password on profile page and err handling

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,16 +1,20 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect, useRef } from "react";
 import { GhibliContext } from "../context/GlobalContext";
-import { updateUser } from "../api";
+import { updateUser, updatePassword } from "../api";
 import { imgURL } from "../constants";
+import ErrorMessage from "../components/ErrorMessage";
 
 const ProfilePage = () => {
   const {
     user: { name, photo, email },
     setUser,
-    token,
+    setError,
   } = useContext(GhibliContext);
 
   const [avatar, setAvatar] = useState();
+  const currPsw = useRef();
+  const psw = useRef();
+  const pswConfirm = useRef();
   // TODO: to remove previous image url from db
   // const [prevAvatar, setPrevAvatar] = useState();
 
@@ -28,8 +32,9 @@ const ProfilePage = () => {
       form.append("name", name);
       form.append("email", email);
       form.append("photo", photo);
-      const { data } = await updateUser(form, token);
+      const { data } = await updateUser(form);
       setUser(data.data.user);
+      localStorage.setItem("user", JSON.stringify(data.data.user));
       // now we should remove the previous avatar from our db
       // use fs.unlink() async one to do the work
     } catch (e) {
@@ -37,10 +42,33 @@ const ProfilePage = () => {
     }
   };
 
+  const handleChangePsw = async (e) => {
+    e.preventDefault();
+    const payload = {
+      passwordCurrent: currPsw.current?.value,
+      password: psw.current?.value,
+      passwordConfirm: pswConfirm.current?.value,
+    };
+    try {
+      const result = await updatePassword(payload);
+      console.log(result);
+    } catch (e) {
+      console.error(e.response);
+      const {
+        data: { message },
+      } = e.response;
+      setError({
+        hidden: false,
+        message: message,
+      });
+    }
+  };
+
   return (
-    <div className="h-fullHeight">
+    <div className="">
+      <ErrorMessage />
       <form
-        className="pt-5 flex-col px-20 h-fullHeight"
+        className="pt-5 flex-col px-20 pb-10 border-b border-light"
         onSubmit={handleSubmit}
       >
         <div className="flex-col ">
@@ -48,7 +76,7 @@ const ProfilePage = () => {
             Name:
           </label>
           <input
-            className="p-3 bg-blue-100 rounded-md w-full my-3 pointer-events-none"
+            className="profileInput pointer-events-none"
             id="name"
             type="text"
             name="name"
@@ -60,7 +88,7 @@ const ProfilePage = () => {
             Email Address
           </label>
           <input
-            className="p-3 bg-blue-100 rounded-md w-full my-3 pointer-events-none"
+            className="profileInput pointer-events-none"
             id="email"
             type="text"
             name="email"
@@ -69,17 +97,55 @@ const ProfilePage = () => {
         </div>
         <div>
           <img className="h-28" alt="user" src={avatar} />
+          <p>{photo}</p>
           <input type="file" accept="image/*" id="photo" name="photo" />
-          <label className="block" htmlFor="photo">
-            Choose new photo
-          </label>
         </div>
 
-        <button
-          type="submit"
-          className="p-5 bg-green-300 rounded-md block ml-auto text-white font-bold"
-        >
+        <button type="submit" className="primaryBtn">
           Save Settings
+        </button>
+      </form>
+
+      <form className="pt-5 flex-col px-20 pb-10" onSubmit={handleChangePsw}>
+        <div className="flex-col ">
+          <label className="block" htmlFor="currPsw">
+            Current Password
+          </label>
+          <input
+            className="profileInput"
+            id="currPsw"
+            type="password"
+            name="currPsw"
+            ref={currPsw}
+          />
+        </div>
+        <div className="flex-col">
+          <label className="block" htmlFor="psw1">
+            New Password
+          </label>
+          <input
+            className="profileInput"
+            id="psw1"
+            type="password"
+            name="psw1"
+            ref={psw}
+          />
+        </div>
+        <div>
+          <label className="block" htmlFor="psw2">
+            Confirm Password
+          </label>
+          <input
+            className="profileInput"
+            id="psw2"
+            type="password"
+            name="psw2"
+            ref={pswConfirm}
+          />
+        </div>
+
+        <button type="submit" className="primaryBtn">
+          Update Password
         </button>
       </form>
     </div>
