7906f2285eaf634968274424c4af26b67e83bfc8
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-27 00:16:20 +0100

    Add redirect to frontpage after 3 sec

--- client/src/pages/Page404.jsx
+++ client/src/pages/Page404.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 
 const Page404 = () => {
+  const history = useHistory();
+  useEffect(() => {
+    const time = setTimeout(() => {
+      history.push("/");
+    }, 3000);
+    return () => {
+      clearTimeout(time);
+    };
+  }, []);
+
   return (
-    <div className='textCenter'>
-      The page you are viewing does not exist.
+    <div className="textCenter min-h-fullHeight flex-col">
+      <h1 className="mb-6">The page you are viewing does not exist.</h1>
+      <p>You will be redirected to landing page in 3 seconds.</p>
     </div>
-  )
-}
+  );
+};
 
 export default Page404;
