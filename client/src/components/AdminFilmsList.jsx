ac212a424d3c9ecb874aea233bc564a423eff28f
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-26 11:38:44 +0100

    Admin: delete film

--- client/src/components/AdminFilmsList.jsx
+++ client/src/components/AdminFilmsList.jsx
@@ -2,17 +2,35 @@ import React from "react";
 import { BiEdit } from "react-icons/bi";
 import { RiDeleteBin5Fill } from "react-icons/ri";
 import { Link, useHistory } from "react-router-dom";
+import "react-confirm-alert/src/react-confirm-alert.css";
+import { GenerateConfirmUI } from "../utilities";
+import { deleteFilmByAdmin } from "../api";
 
-const AdminFilmsList = ({ film }) => {
+const AdminFilmsList = ({ film, setIsDeleted, isDeleted }) => {
   const history = useHistory();
 
   const handleEditFilm = () => {
     history.push(`/admin/films/${film.slug}/edit`);
   };
 
+  const handleDeleteFilm = async () => {
+    try {
+      // console.log(user._id);
+      const text = "You want to delete this film?";
+      const action = async () => {
+        await deleteFilmByAdmin(film._id);
+        setIsDeleted(!isDeleted);
+      };
+      GenerateConfirmUI(text, action);
+      // const res = await deleteUserByAdmin(user._id)
+    } catch (e) {
+      console.error(e.response);
+    }
+  };
+
   return (
     <tr className="h-10 transform transition duration-300 hover:bg-red-50 ">
-      <td className="">{film.title}</td>
+      <td className="pl-5">{film.title}</td>
       <td className="text-center">
         {new Date(film.createdAt).toLocaleString()}
       </td>
@@ -20,14 +38,17 @@ const AdminFilmsList = ({ film }) => {
         <Link
           to={{
             pathname: `/admin/films/${film.slug}/edit`,
-            filmProps: { film: film },
+            filmProps: { film },
           }}
           className="primaryBtn my-3 ml-0 flex items-center"
           onClick={handleEditFilm}
         >
           <BiEdit className="m-auto" />
         </Link>
-        <button className="primaryBtn my-3 ml-0 bg-indigo-300">
+        <button
+          className="primaryBtn my-3 ml-0 bg-indigo-300"
+          onClick={handleDeleteFilm}
+        >
           <RiDeleteBin5Fill className="cursor-pointer" />
         </button>
       </td>
