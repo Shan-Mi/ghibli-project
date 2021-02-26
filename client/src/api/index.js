e9268552c1c710d47f7e3de86b0e577390c10f64
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-26 09:43:05 +0100

    Format func structure

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -8,12 +8,15 @@ const privateHeaders = (token) => ({
     Authorization: `Bearer ${token}`,
   },
 });
-
+/* films */
 export const fetchFilms = async () => await axios.get(`${URL}films/`);
 
 export const fetchFilm = (id) => axios.get(`${URL}${id}`);
 
-export const deletePost = (id) => axios.delete(`${URL}/${id}`);
+export const updateFilm = async (payload, filmId) =>
+  await axios.patch(`${URL}films/${filmId}`, payload);
+
+/* auth */
 
 export const login = async (payload) =>
   await axios.post(`${URL}users/login`, payload);
@@ -33,11 +36,22 @@ export const resetPassword = async (payload, token) =>
     privateHeaders(token)
   );
 
+export const sendVerifyEmail = async (payload) =>
+  await axios.post(`${URL}users/verifyEmail`, payload);
+
+export const updatePassword = async (payload) =>
+  await axios.patch(`${URL}users/updateMyPassword`, payload);
+
+/* helper */
 export const getErrorMessage = (e) => e.response?.data.message;
 
+/* user */
 export const updateUser = async (payload) =>
   await axios.patch(`${URL}users/updateMe`, payload);
 
+export const getAllUsers = async () => await axios.get(`${URL}users`);
+
+/* review */
 // in payload, should have {userId, filmId, title, content}
 export const createNewReview = async (payload, filmId) =>
   await axios.post(`${URL}films/${filmId}/reviews/`, payload);
@@ -48,18 +62,14 @@ export const updateReview = async (payload, filmId, reviewId) =>
 export const likeReview = async (filmId, reviewId) =>
   await axios.patch(`${URL}films/${filmId}/reviews/${reviewId}/likeReview`);
 
-export const sendVerifyEmail = async (payload) =>
-  await axios.post(`${URL}users/verifyEmail`, payload);
-
-export const updatePassword = async (payload) =>
-  await axios.patch(`${URL}users/updateMyPassword`, payload);
-
 export const getAllReviews = async () => await axios.get(`${URL}reviews`);
 
-export const getAllUsers = async () => await axios.get(`${URL}users`);
-
-export const updateFilm = async (payload, filmId) =>
-  await axios.patch(`${URL}films/${filmId}`, payload);
-
+/* admin delete - irrevocable */
 export const deleteUserByAdmin = async (userId) =>
   await axios.delete(`${URL}users/${userId}`);
+
+export const deleteReviewByAdmin = async (reviewId) =>
+  await axios.delete(`${URL}reviews/${reviewId}`);
+
+export const deleteFilmByAdmin = async (filmId) =>
+  await axios.delete(`${URL}films/${filmId}`);
