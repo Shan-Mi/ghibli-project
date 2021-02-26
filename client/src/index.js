b826fd5b7abafd21cd41051f6e2e0aef055f321f
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-27 00:11:00 +0100

    Move toastify's css file to root level

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import "react-confirm-alert/src/react-confirm-alert.css";
-import "antd/dist/antd.css";
+import "react-toastify/dist/ReactToastify.css";
+
+// import "antd/dist/antd.css";
 import App from "./App";
 import axios from "axios";
 import reportWebVitals from "./reportWebVitals";
