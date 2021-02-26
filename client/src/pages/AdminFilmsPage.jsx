72751f78490c54afc0395bce149ec917d9f752bb
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-26 11:42:17 +0100

    Admin: films update correctly if any changes made

--- client/src/pages/AdminFilmsPage.jsx
+++ client/src/pages/AdminFilmsPage.jsx
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from "react";
+import { fetchFilms } from "../api";
 import AdminEditGroup from "../components/AdminEditGroup";
 import AdminFilmsList from "../components/AdminFilmsList";
 import AdminGoBackBtn from "../components/AdminGoBackBtn";
+import { IoMdAddCircleOutline } from "react-icons/io";
+import { Link } from "react-router-dom";
 
 const AdminFilmsPage = () => {
-  const [films, setFilms] = useState(JSON.parse(localStorage.getItem("films")));
+  const [films, setFilms] = useState();
   const [update, setUpdate] = useState(false);
+  const [isDeleted, setIsDeleted] = useState(false);
 
   useEffect(() => {
     setFilms(films);
   }, [films]);
+
+  useEffect(() => {
+    fetchFilms().then((res) => setFilms(res.data.data.films));
+  }, [isDeleted]);
 
   const sortBy = (query) => {
     if (update) {
@@ -26,7 +34,18 @@ const AdminFilmsPage = () => {
         <AdminGoBackBtn marginR="mr-20" location="/admin" />
         <AdminEditGroup />
       </div>
-      <h1 className="adminTitle">Edit Films</h1>
+      <div className="relative">
+        <h1 className="adminTitle">Edit Films</h1>
+        <Link
+          className="absolute top-6 right-20 text-2xl text-dark flex items-center transform hover:text-blue-900 hover:scale-105  hover:text-opacity-40 duration-200 ease-in-out"
+          to="/admin/films/create"
+        >
+          <IoMdAddCircleOutline className="mr-5" />
+          <span className="font-thin font-Montserrat text-xl ">
+            Add a new film
+          </span>
+        </Link>
+      </div>
       <table className="table-fixed w-11/12 font-Montserrat mt-10 m-auto">
         <thead>
           <tr className="text-xl h-20">
@@ -48,7 +67,12 @@ const AdminFilmsPage = () => {
         <tbody>
           {films &&
             films.map((film, index) => (
-              <AdminFilmsList film={film} key={`film-${index}`} />
+              <AdminFilmsList
+                film={film}
+                key={`film-${index}`}
+                setIsDeleted={setIsDeleted}
+                isDeleted={isDeleted}
+              />
             ))}
         </tbody>
       </table>
