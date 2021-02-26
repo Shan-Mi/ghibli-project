2eda9abf51806bea5bdc47e3c9c11343a19f41fa
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-26 11:43:13 +0100

    Admin: preparation and misc tweak

---
+++ client/src/pages/AdminCreateFilmPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminCreateFilmPage = () => {
+  return (
+    <div className='min-h-fullHeight'>
+      This is create new film page.
+    </div>
+  )
+}
+
+export default AdminCreateFilmPage
--- client/src/pages/FrontPage.jsx
+++ client/src/pages/FrontPage.jsx
@@ -14,20 +14,26 @@ const FrontPage = () => {
       } = await fetchFilms();
       const filmsData = films.films;
       setFilms(filmsData);
-      localStorage.setItem("films", JSON.stringify(filmsData));
+      // localStorage.setItem("films", JSON.stringify(filmsData));
     };
     getFilms();
   }, [setFilms]);
 
   return (
-    <main className="flex-grow flex justify-center items-center">
-      <div className="mx-auto px-4 sm:px-8 py-2 text-center">
-        <div className="grid grid-cols-6 gap-4 items-start mt-8 mx-auto px-8 max-w-7xl">
-          {films.map((film, index) => (
-            <FilmReview film={film} key={index} />
-          ))}
+    <main className="flex-grow flex justify-center items-center min-h-fullHeight">
+      {films.length !== 0 ? (
+        <div className="mx-auto px-4 sm:px-8 py-2 text-center">
+          <div className="grid grid-cols-6 gap-4 items-start mt-8 mx-auto px-8 max-w-7xl">
+            {films.map((film, index) => (
+              <FilmReview film={film} key={index} />
+            ))}
+          </div>
         </div>
-      </div>
+      ) : (
+        <h1 className="text-3xl font-Amaranth italic">
+          Something went wrong, please try again later.
+        </h1>
+      )}
     </main>
   );
 };
