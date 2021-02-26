355d254e978bf1b7e93f11f5225095658979d4f7
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-02-27 00:16:03 +0100

    Tweak style, min-h-fullHeight

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import LoginForm from "../components/LoginForm";
 
 const LoginPage = () => {
   return (
-    <div className='flex-1 h-fullHeight'>
+    <div className='flex-1 min-h-fullHeight'>
       <LoginForm />
     </div>
   );
