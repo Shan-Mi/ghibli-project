df9846416a326ef7605845625b9338e1d1f60561
Shan-Mi <shan.mi.fanfan@gmail.com>
2021-03-02 22:58:56 +0100

    Client: add fall back for broken image

--- client/src/components/FilmReview.jsx
+++ client/src/components/FilmReview.jsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { filmImgURL } from "../constants";
 
 const FilmReview = ({ film }) => {
   const { imageCover, title, runtime, rating, slug } = film;
+  const [image, setImage] = useState(`${filmImgURL}${imageCover}`);
 
   return (
     <div className="col-span-6 sm:col-span-6 md:col-span-3 lg:col-span-2 xl:col-span-2 max-w-xl transform hover:scale-105 duration-200 ease-in-out">
@@ -12,7 +14,8 @@ const FilmReview = ({ film }) => {
             className="mx-auto bg-gray-200 rounded-md sm:object-fill sm:h-auto sm:w-auto
             md:h-128 md:w-full md:object-cover md:object-center
             lg:object-cover lg:h-128 lg:w-full lg:object-center"
-            src={imageCover}
+            onError={() => setImage(`${filmImgURL}default.png`)}
+            src={image}
             alt={title}
           />
           <h2 className="text-xl h-20 pt-8 text-center text-gray-700 capitalize p-3 font-Montserrat">
